915e657254c89a84ec9e0f5bf9d582c93977a371
andersaction <a.ullnaess@action.nl>
2024-12-01 19:04:42 +0100

    Implement day 1 solution

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,20 +1,36 @@
+import kotlin.math.abs
+
 fun main() {
+    fun parseNumberLists(input: List<String>): Pair<List<Int>, List<Int>> {
+        val leftList = mutableListOf<Int>()
+        val rightList = mutableListOf<Int>()
+        input.forEach { line ->
+            val parts = line.split("   ").map { it.toInt() }
+            leftList.add(parts[0])
+            rightList.add(parts[1])
+        }
+
+        return Pair(leftList.sorted(), rightList.sorted())
+    }
+
     fun part1(input: List<String>): Int {
-        return input.size
+        val (leftList, rightList) = parseNumberLists(input)
+        return leftList.mapIndexed { index, number ->
+            abs(rightList[index] - number)
+        }.sum()
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val (leftList, rightList) = parseNumberLists(input)
+        return leftList.sumOf { number ->
+            number * rightList.count { it == number }
+        }
     }
 
-    // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
-
-    // Or read a large test input from the `src/Day01_test.txt` file:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 11)
+    check(part2(testInput) == 31)
 
-    // Read the input from the `src/Day01.txt` file.
     val input = readInput("Day01")
     part1(input).println()
     part2(input).println()
